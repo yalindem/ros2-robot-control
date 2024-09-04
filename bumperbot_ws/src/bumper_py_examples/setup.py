02eeba4deabea4cbe34576888a5df9166e6280a0
yalin-ubuntu-desktop <yalindemir@outlook.de>
2024-09-04 23:03:00 +0200

    creat a ros2 py package

---
+++ bumperbot_ws/src/bumper_py_examples/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'bumper_py_examples'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yalin',
+    maintainer_email='yalindemir@outlook.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
