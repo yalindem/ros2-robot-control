975486707c3042333382e79fd612b5c117df6f04
yalin-ubuntu-desktop <yalindemir@outlook.de>
2024-09-05 20:56:28 +0200

    defining simple parameters node in python

--- bumperbot_ws/src/bumperbot_py_examples/setup.py
+++ bumperbot_ws/src/bumperbot_py_examples/setup.py
@@ -21,7 +21,8 @@ setup(
     entry_points={
         'console_scripts': [
             'simple_publisher = bumperbot_py_examples.simple_publisher:main',
-            'simple_subscriber = bumperbot_py_examples.simple_subscriber:main'
+            'simple_subscriber = bumperbot_py_examples.simple_subscriber:main',
+            'simple_parameters = bumperbot_py_examples.simple_parameters:main'
         ],
     },
 )
