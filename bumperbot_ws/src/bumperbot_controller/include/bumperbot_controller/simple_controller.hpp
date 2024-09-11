631490c1acb343016eaf396c2071e6d1ad3674d4
yalin-ubuntu-desktop <yalindemir@outlook.de>
2024-09-12 00:34:32 +0200

    publishing odom msg

--- bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
+++ bumperbot_ws/src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
@@ -5,6 +5,8 @@
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <std_msgs/msg/float64_multi_array.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+
 #include <Eigen/Core>
 
 class SimpleController : public rclcpp::Node
@@ -19,6 +21,7 @@ class SimpleController : public rclcpp::Node
         rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
         rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
         rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_sub_;
+        rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
 
         double wheel_radius_;
         double wheel_seperation_;
@@ -33,6 +36,8 @@ class SimpleController : public rclcpp::Node
         double x_;
         double y_;
         double theta_;
+
+        nav_msgs::msg::Odometry odom_msg_;
 
 };
 
--- bumperbot_ws/src/bumperbot_controller/src/simple_controller.cpp
+++ bumperbot_ws/src/bumperbot_controller/src/simple_controller.cpp
@@ -1,5 +1,6 @@
 #include "bumperbot_controller/simple_controller.hpp"
 #include <Eigen/Geometry>
+#include <tf2/LinearMath/Quaternion.h>
 
 using std::placeholders::_1;
 
@@ -21,11 +22,18 @@ SimpleController::SimpleController(const std::string& name) : Node(name), left_w
                     std::bind(&SimpleController::velCallback, this, _1));
     
     joint_sub_ = create_subscription<sensor_msgs::msg::JointState>("/joint_states", 10, std::bind(&SimpleController::jointCallback, this, _1));
-
+    odom_pub_ = create_publisher<nav_msgs::msg::Odometry>("bumperbot_controller/odom", 10);
     speed_conversion_(0, 0) = wheel_radius_ / 2;
     speed_conversion_(0, 1) = wheel_radius_ / 2;
     speed_conversion_(1, 0) = wheel_radius_ / wheel_seperation_;
     speed_conversion_(1, 1) = -wheel_radius_ / wheel_seperation_;
+
+    odom_msg_.header.frame_id = "odom";
+    odom_msg_.child_frame_id = "base_footprint";
+    odom_msg_.pose.pose.orientation.x = 0.0;
+    odom_msg_.pose.pose.orientation.y = 0.0;
+    odom_msg_.pose.pose.orientation.z = 0.0;
+    odom_msg_.pose.pose.orientation.w = 1.0;
     RCLCPP_INFO_STREAM(get_logger(), "The conversion matrix is: \n" << speed_conversion_);
 }
 
@@ -66,6 +74,19 @@ void SimpleController::jointCallback(const sensor_msgs::msg::JointState& msg)
     x_ += d_s * std::cos(theta_);
     y_ += d_s * std::sin(theta_);
 
+    tf2::Quaternion q;
+    q.setRPY(0,0,theta_);
+    odom_msg_.pose.pose.orientation.x = q.getX();
+    odom_msg_.pose.pose.orientation.y = q.getY();
+    odom_msg_.pose.pose.orientation.z = q.getZ();
+    odom_msg_.pose.pose.orientation.w = q.getW();
+    odom_msg_.header.stamp = get_clock()->now();
+    odom_msg_.pose.pose.position.x = x_;
+    odom_msg_.pose.pose.position.y = y_;
+    odom_msg_.twist.twist.linear.x = linear;
+    odom_msg_.twist.twist.angular.z = angular;
+
+    odom_pub_->publish(odom_msg_);
     RCLCPP_INFO_STREAM(get_logger(), "linear: " << linear << " Angular: " << angular);
     RCLCPP_INFO_STREAM(get_logger(), "x: " << x_ << " y_: " << y_ << "theta: " << theta_);
 }
